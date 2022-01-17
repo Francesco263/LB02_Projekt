565be9128a58da34c80bb5be36502788d3c05044
Philip Jovanovic <jovanovicp@bzz.ch>
2022-01-17 14:38:54 +0100

    bubble sort v.1

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,62 +1,46 @@
+public class QuickSort extends Algorithm{
 
-import java.util.*;
 
-class QuickSort {
+    @Override
+    public void sort(int[] array) {
 
-    static void sort(int arr[], int low, int high) {
+        int n = array.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (array[j] > array[j+1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+        printArray(array);
 
-        Random rand = new Random();
-        int pivot = rand.nextInt(high - low) + low;
-
-        int index1 = arr[pivot];
-        arr[pivot] = arr[high];
-        arr[high] = index1;
     }
-
-    static int partition(int arr[], int low, int high) {
-       sort(arr, low, high);
-        int pivot = arr[high];
-
-
-        int i = (low - 1);
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-
-        return i + 1;
-    }
-
-
-    static void _sort(int arr[], int low, int high) {
-        if (low < high) {
-
-
-            int pi = partition(arr, low, high);
-
-
-            _sort(arr, low, pi - 1);
-            _sort(arr, pi + 1, high);
-        }
-    }
-
-
-    static void printArray(int arr[]) {
+    public void printArray(int arr[])
+    {
         int n = arr.length;
-        for (int i = 0; i < n; ++i)
+        for (int i=0; i<n; ++i)
             System.out.print(arr[i] + " ");
         System.out.println();
     }
 
+    @Override
+    public long getTime() {
+        return 0;
+    }
 
+    @Override
+    public int getComparison() {
+        return 0;
+    }
+
+    @Override
+    public int getArrayAccess() {
+        return 0;
+    }
+
+    @Override
+    public int getStorage() {
+        return 0;
+    }
 }
